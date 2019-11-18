f7ae08beb7eda5d30f0132c83a6736d963871f8d
luismasuelli <luis@triplemint.com>
2019-11-18 17:37:53 -0500

    A restriction for sources (by their timestamps, and the required interval) in digests.

--- package/digests.py
+++ package/digests.py
@@ -6,7 +6,11 @@ from .timelapses import Timelapse
 class Digest(Timelapse):
     """
     Digests can be connected to source frames to summarize their data: they have an interval size which
-      must be BIGGER to the interval size in the referenced source.
+      must be BIGGER to the interval size in the referenced source, and the source must have a datetime
+      which must also be rounded (i.e. exactly divisible) by the given interval (e.g. if a Source using
+      timestamp at date X and time 05:00:00 has an interval size of 1 minute, the allowed intervals
+      for a digest are from 1 minute to 1 hour, while 2 hours, 3 hours, 4 hours, ... are disallowed
+      because 05:00:00 is not divisible by any of them).
     """
 
     def __init__(self, source, interval):
@@ -14,6 +18,9 @@ class Digest(Timelapse):
             raise ValueError("The source for the digest must not be None")
         if not interval.allowed_as_digest(source.interval):
             raise ValueError("The chosen digest interval size must be bigger to the source's interval size")
+        if source.timestamp != interval.round(source.timestamp):
+            raise ValueError("Cannot create a digest for the given interval and source, since the source's start "
+                             "date is not rounded to the given interval size for this digest")
         Timelapse.__init__(self, Candle, interval, 240, 1)
         self._source = source
         self._source.on_refresh_digests.register(self._on_refresh)
@@ -34,7 +41,7 @@ class Digest(Timelapse):
         Implements the timestamp by returning the source's timestamp.
         """
 
-        return self.interval.round(self._source.timestamp)
+        return self._source.timestamp
 
     @property
     def attached(self):
