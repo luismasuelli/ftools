a6e4989acbdaeeb76b3403a36da89bebdb675718
luismasuelli <luis@triplemint.com>
2020-02-28 18:43:46 -0500

    Smart pluckers also include the side plucker.

--- mistra/core/utils/mappers/smart_pluckers.py
+++ mistra/core/utils/mappers/smart_pluckers.py
@@ -1,17 +1,21 @@
 from ...sources import Source
 from ...indicators import Indicator
 from ...pricing import Candle
+from .side_pluckers import SidePlucker
 from .candle_pluckers import CandlePlucker
 from .row_pluckers import RowPlucker
 
 
-def smart_plucker(source, component='end', row=0):
+def smart_plucker(source, side=Source.ASK, component='end', row=0):
     """
     Depending on the source's type, it will be
       smart enough to instantiate a plucker, or
       return the source as-is. In the end, it
       returns a mean to get a width=1 source.
+    Also, if it is a source, it will use a specific
+      side (and not both) to fetch.
     :param source: The source to wrap.
+    :param side: The flow side to pick.
     :param component: The component to pluck.
       Used if the source is Candle-type.
     :param row: The row to pluck.
@@ -21,9 +25,9 @@ def smart_plucker(source, component='end', row=0):
 
     if isinstance(source, Source):
         if source.dtype == Candle:
-            return CandlePlucker(source, component)
+            return CandlePlucker(SidePlucker(source, side), component)
         elif source.dtype == int:
-            return source
+            return SidePlucker(source, side)
         else:
             raise TypeError("The source has an unexpected dtype")
     elif isinstance(source, Indicator):
