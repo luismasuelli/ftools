c25a3658cec7eccb76366c66cef0c2bad91c8acd
Luis Masuelli <luisfmasuelli@gmail.com>
2024-12-01 12:09:20 -0500

    Reformatting

--- setup.py
+++ setup.py
@@ -14,7 +14,8 @@ setup(
     license='MIT',
     author='luismasuelli',
     author_email='luisfmasuelli@gmail.com',
-    description='MISTRA (Market InSights / TRading Algorithms) provides core support to market timelapses and indicators management',
+    description='MISTRA (Market InSights / TRading Algorithms) provides core support to market '
+                'timelapses and indicators management',
     python_requires='>=3.12',
     install_requires=['numpy>=2.1.3', 'requests>=2.22.0', 'scipy>=1.14.1']
 )
