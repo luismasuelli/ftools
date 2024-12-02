d722ffa385a5132c43bf30f6fe3a0a4786c2971c
Luis Masuelli <luisfmasuelli@gmail.com>
2024-12-02 12:14:13 -0500

    Version 0.1.2 for Mistra.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mistra',
-    version='0.1.1',
+    version='0.1.2',
     packages=['mistra.core',
               'mistra.core.growing_arrays',
               'mistra.core.indicators',
