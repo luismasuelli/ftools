013c45803847517609e316c9ccda4a31cd6ce734
Luis Masuelli <luisfmasuelli@gmail.com>
2024-12-03 16:31:26 -0500

    Adding a new microversion.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mistra',
-    version='0.1.2',
+    version='0.1.3',
     packages=['mistra.core',
               'mistra.core.growing_arrays',
               'mistra.core.indicators',
