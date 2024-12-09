14be7ed6e7a30be7e9bcf5a34b40635b618d61f9
Luis Masuelli <luisfmasuelli@gmail.com>
2024-12-09 16:26:54 -0500

    Using version 0.1.9.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mistra',
-    version='0.1.8',
+    version='0.1.9',
     packages=['mistra.core',
               'mistra.core.growing_arrays',
               'mistra.core.indicators',
